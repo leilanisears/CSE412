e7ceb85fb8a290635dd253cb9d865400fc8d5400
leilanisears <lsears1@asu.edu>
2021-11-27 11:07:21 -0700

    some orginanization

--- data_importer.sql
+++
@@ -1,3 +0,0 @@
-COPY playlist FROM '/home/leisea/school/412/database/project/Playlist.csv' DELIMITER ',' CSV HEADER;
-COPY userentity FROM '/home/leisea/school/412/database/project/UserEntity.csv' DELIMITER ',' CSV HEADER;
-COPY song FROM '/home/leisea/school/412/database/project/Song.csv' DELIMITER ',' CSV HEADER;
