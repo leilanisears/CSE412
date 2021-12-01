172ee9c84aaf764333bb1121b6ffa4d1066077a7
Owen O'Toole <ootoole@asu.edu>
2021-12-01 11:10:08 -0800

    Minor url routing changes

--- final_project/musicdb_project/musicdb_project/urls.py
+++ final_project/musicdb_project/musicdb_project/urls.py
@@ -30,8 +30,7 @@ urlpatterns = [
     # path('u/', user_views.profile, name='user-profile'),
     # path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     # path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path("playlists/", include("musicdb.urls")),
-    path('users/', include("users.urls")),
+    path('u/', include("users.urls")),
     path('contact/', include("contact.urls")),
     #path('search/', include("search.urls")),
 
--- final_project/musicdb_project/users/urls.py
+++ final_project/musicdb_project/users/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path("login/", login, name="login"),
     path("logout/", logout, name="logout"),
     path("register/", register, name="register"),
-    path('u/<str:user_id>/', profile, name="profile"),
+    path('<str:user_id>/', profile, name="profile"),
     path("delete/<str:user_id>", delete_profile, name="delete-profile"),
     path("<str:user_id>/<str:action>/", follow_unfollow, name="follow-unfollow"),
 ]
