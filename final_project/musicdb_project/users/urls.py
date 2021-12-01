f9ce2b91f7a6cccc06dd57f04b01b78b8e74b42b
leilanisears <lsears1@asu.edu>
2021-11-30 19:51:31 -0700

    fixed url parameters

--- final_project/musicdb_project/users/urls.py
+++ final_project/musicdb_project/users/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     #path("login/", views.login, name="login"),
     #path("<user_id>/logout", views.logout, name="logout"),
     #path("register/", views.register_request, name="register"),
-    path("edit/", views.edit_profile, name="edit profile"),
-    path("delete/", views.delete_profile, name="delete profile"),
-    path("<user_id>/follow_unfollow", views.follow_unfollow, name="follow or unfollow"),
+    path("edit/<str:user_id>/", views.edit_profile, name="edit profile"),
+    path("delete/<str:user_id>", views.delete_profile, name="delete profile"),
+    path("<str:user_id>/<str:action>/", views.follow_unfollow, name="follow or unfollow"),
 ]
