1e29b6e2e594ce71170ade1c4e508510b1d40a4e
Owen O'Toole <ootoole@asu.edu>
2021-12-02 20:04:35 -0800

    Fix URLs

--- final_project/musicdb_project/musicdb_project/urls.py
+++ final_project/musicdb_project/musicdb_project/urls.py
@@ -18,23 +18,23 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import TemplateView
-#from users import views as user_views
-#from musicdb import views as music_views
+from users import views as user_views
+from musicdb import views as music_views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    #path('welcome/', include("musicdb.urls")),
+    # path('welcome/', include("musicdb.urls")),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    #path('register/', user_views.register, name='register'),
-    #path('account/', user_views.profile, name='user-profile'),
-    #path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    #path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
-    #path('create/', music_views.PlaylistCreateView.as_view(template_name='playlist_create.html'), name='playlist-create'),
-    #path("u/<str:username>/", music_views.UserPlaylistView.as_view(), name="user-playlists"),
-    #path('contact/', include("contact.urls")),
-    #path('search/', include("search.urls")),
-    #path('', include('musicdb.urls')),
+    path('register/', user_views.register, name='register'),
+    path('account/', user_views.profile, name='user-profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('create/', music_views.PlaylistCreateView.as_view(template_name='playlist_create.html'), name='playlist-create'),
+    path("u/<str:username>/", music_views.UserPlaylistView.as_view(), name="user-playlists"),
+    path('contact/', include("contact.urls")),
+    # path('search/', include("search.urls")),
+    path('', include('musicdb.urls')),
 ]
 
 if settings.DEBUG:
